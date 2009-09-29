98ec4cfc4c5334c13c23369c2f1bf91a5736cae2
Sam Rosen <rosenboy@Momentum.local>
2009-09-29 11:02:54 -0500

    styling, iframes, active links, sub nav, getting pretty damn close

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,28 @@
+function loadTweets() {
+  $(".twitter_feed").getTwitter({
+    userName: "rosenboy",
+    numTweets: 1,
+    loaderText: "Twittering Twitter...",
+    slideIn: false,
+    showHeading: false,
+    showProfileLink: false
+  });
+  Cufon.replace('.twitter_feed span');
+  Cufon.now();
+}
+
+
 $(document).ready(function(){
+
+  loadTweets();
+
   Cufon.replace('.nav a');
   Cufon.replace('h1 a');
   Cufon.replace('.headline');
   Cufon.replace('#content')
   
-  $("#toolbar .hidden_bar").hide();
+  
+  $("body.home #toolbar .hidden_bar").hide();
 
   hiConfig = {
     sensitivity: 2, // number = sensitivity threshold (must be 1 or higher)
@@ -15,9 +33,10 @@ $(document).ready(function(){
       $("#toolbar .hidden_bar div").fadeIn('fast');
     }, // function = onMouseOver callback (REQUIRED)
     out: function() { 
-      $("#toolbar .hidden_bar div").hide();
+      $("#toolbar .hidden_bar div").hide('fast');
       $("#toolbar .hidden_bar").slideUp(); 
     } // function = onMouseOut callback (REQUIRED)
    }
-   $('#toolbar').hoverIntent(hiConfig)
+   $('#toolbar').hoverIntent(hiConfig);
+
 });
