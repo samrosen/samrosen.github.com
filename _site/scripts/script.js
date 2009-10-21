05007381bbae5201c08ca91691801684978c5173
Sam Rosen <rosenboy@Momentum.local>
2009-10-20 23:05:13 -0500

    fucked up nav and possibly working 404 page, who knows

--- _site/scripts/script.js
+++ _site/scripts/script.js
@@ -16,12 +16,7 @@ $(document).ready(function(){
 
   loadTweets();
 
-  Cufon.replace('.nav a');
-  Cufon.replace('h1 a');
-  Cufon.replace('.headline');
-  Cufon.replace('#content')
-  
-  
+  Cufon.replace('.nav a, h1 a, .headline, #content');  
   $("body.home #toolbar .hidden_bar").hide();
 
   hiConfig = {
@@ -29,12 +24,10 @@ $(document).ready(function(){
     interval: 0, // number = milliseconds for onMouseOver polling interval
     timeout: 400, // number = milliseconds delay before onMouseOut
     over: function() {
-      $("#toolbar .hidden_bar").slideDown();
-      $("#toolbar .hidden_bar div").fadeIn('fast');
+
     }, // function = onMouseOver callback (REQUIRED)
     out: function() { 
-      $("#toolbar .hidden_bar div").hide('fast');
-      $("#toolbar .hidden_bar").slideUp(); 
+
     } // function = onMouseOut callback (REQUIRED)
    }
    $('#toolbar').hoverIntent(hiConfig);
