6a13558cb713d5e7278692dfcc45e1461be3214a
Justin Knowlden <gus@gusg.us>
2009-10-21 18:50:47 -0500

    Some minor work to get the drop down nav working, I think

--- scripts/script.js
+++ scripts/script.js
@@ -17,35 +17,34 @@ $(document).ready(function(){
   loadTweets();
 
   Cufon.replace('.nav a, h1 a, .headline, #content');  
-  $("body.home #toolbar .hidden_bar").hide();
 
   hiConfig = {
     sensitivity: 2, // number = sensitivity threshold (must be 1 or higher)
     interval: 0, // number = milliseconds for onMouseOver polling interval
     timeout: 400, // number = milliseconds delay before onMouseOut
     over: function() {
-
+      $(".hidden_bar").show();
     }, // function = onMouseOver callback (REQUIRED)
     out: function() { 
-
+      $(".hidden_bar").hide();
     } // function = onMouseOut callback (REQUIRED)
-   }
-   $('#toolbar').hoverIntent(hiConfig);
+  }
+  $('#toolbar').hoverIntent(hiConfig);
+
+  $(".hidden_bar").hide();
    
-   $(".slideshow").jCarouselLite({
-       btnNext: ".next",
-       btnPrev: ".prev",
-       visible: 1,
-       speed: 700,
-       mouseWheel: true
-   });
-   
-   $('#content .slideshow li').live("mouseover", function(){
-     var buttons = $("button")
-     buttons.fadeIn();
-     $(this).mouseout(function(){
-       buttons.fadeOut('slow');
-     })
-   })
+  $(".slideshow").jCarouselLite({
+    btnNext: ".next",
+    btnPrev: ".prev",
+    visible: 1,
+    speed: 700,
+    mouseWheel: true
+  });
+
+  $('#content .slideshow li').live("mouseover", function() {
+    var buttons = $("button")
+    buttons.fadeIn();
+    $(this).mouseout(function() { buttons.fadeOut('slow'); });
+  })
 
 });
